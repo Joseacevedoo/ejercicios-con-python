292153647942588292e33dc04911883d903810c9
Jose Acevedo <joseacv40@gmail.com>
2022-11-16 10:44:07 -0300

    Practica

--- funciones.py
+++ funciones.py
@@ -1,7 +1,7 @@
 def operaciones (): #asi se define una funcion, para hacer una invocacion y q no haya rep
     print (5+5)
 
-print("HOLA TROLO")
+print("HOLA QUERIDO")
 operaciones ()
 
 print ("holisssss")
